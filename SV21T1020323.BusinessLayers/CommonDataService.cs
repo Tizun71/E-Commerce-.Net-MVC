0b396a186c78d35bc410a340eb9e711ddb6db558
Tizun71 <gnud0701@gmail.com>
2024-07-18 18:18:01 +0700

    web-first-build

---
+++ SV21T1020323.BusinessLayers/CommonDataService.cs
@@ -0,0 +1,30 @@
+﻿using SV21T1020323.DataLayers;
+using SV21T1020323.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV21T1020323.BusinessLayers
+{
+    public static class CommonDataService
+    {
+        private static readonly CustomerDAL customerDB;
+
+        static CommonDataService()
+        {
+            string connectionString = @"server=.;
+                                        user id=sa;
+                                        password=123456789;
+                                        database=LiteCommerceDB_2023;
+                                        TrustServerCertificate=true";
+            customerDB = new CustomerDAL(connectionString);
+        }
+
+        public static List<Customer> ListOfCustomers()
+        {
+            return customerDB.List();
+        }
+    }
+}
