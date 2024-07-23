f4244b67e242afea0f0cc430f1d1e902bd5bd9a3
Tizun71 <gnud0701@gmail.com>
2024-07-23 09:02:38 +0700

    Customer Edit and Delete View

---
+++ SV21T1020323.DataLayers/CategoriesDAL.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SV21T1020323.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV21T1020323.DataLayers
+{
+    public class CategoriesDAL
+    {
+        private string connectionString = "";
+        public CategoriesDAL(string connectionString) {
+            this.connectionString = connectionString;
+        }
+        /// <summary>
+        /// Get All Categories Data.
+        /// Query database and get data return to the function
+        /// </summary>
+        /// <returns></returns>
+        public List<Categories> List()
+        {
+            List<Categories> list = new List<Categories>();
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                var sql = @"SELECT * FROM Categories";
+                list = connection.Query<Categories>(sql: sql, commandType:System.Data.CommandType.Text).ToList();
+                connection.Close();
+            }
+            return list;
+        }
+    }
+}
---
+++ SV21T1020323.DomainModels/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV21T1020323.DomainModels
+{
+    /// <summary>
+    /// Categories Data
+    /// </summary>
+    public class Categories
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string CategoryDescription { get; set; } = string.Empty ;
+    }
+}
